f4313bab3889fd7d5ebcad43213b29b84bb86817
ketig <41895005+ketig@users.noreply.github.com>
2024-04-14 18:46:10 +0400

    .

---
+++ script.js
@@ -0,0 +1,29 @@
+function addItem() {
+    let task = document.getElementById("task-input").value;
+
+    if (task === '') {
+        alert("Please enter a task!");
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = task;
+        document.getElementById("tasks").appendChild(li);
+
+        // შევქმნათ span ელემენტი
+        let span = document.createElement('span');
+        // &#x2715 - ამას ჩავწერთ სპანში
+        span.innerHTML = '&#x2715';
+        // li-ს მივაბამთ span-ს
+        li.appendChild(span);
+
+        document.getElementById("task-input").value = '';
+    }
+}
+
+setTimeout(() => {
+    document.getElementById("tasks").addEventListener('click', function (e) {
+    if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+}, 1000);
